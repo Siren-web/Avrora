2c11d632694e1ef9bb4cf84fa1410bd0d0e59b4f
Nick Maksimov <nickstogramm@yandex.ru>
2024-07-28 13:08:09 +0700

    Rename to Avrora

--- Avrora.py
+++ Avrora.py
@@ -1,22 +1,22 @@
 """
-python -m PyInstaller --onefile athena.py
+python -m PyInstaller --onefile Avrora.py
 """
 
 from core import Core
 
 debug_mode = True
 
-start_athena = Core(user_config="./libs/config/user_config.yaml")
+start_avrora = Core(user_config="./libs/config/user_config.yaml")
 
 # connector
-start_athena.video_btn()
+start_avrora.video_btn()
 
 
-@start_athena.bot.message_handler(content_types=['text'])
+@start_avrora.bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    global start_athena
+    global start_avrora
 
-    start_athena.voices(message.text, message.chat.id)
+    start_avrora.voices(message.text, message.chat.id)
 
 
 def main():
@@ -24,11 +24,11 @@ def main():
         try:
             print("Bot is started...")
 
-            global start_athena
+            global start_avrora
 
-            start_athena.info()
-            start_athena.bot.message_handler(content_types=['text'])
-            start_athena.bot.polling(none_stop=True, interval=0)
+            start_avrora.info()
+            start_avrora.bot.message_handler(content_types=['text'])
+            start_avrora.bot.polling(none_stop=True, interval=0)
 
         except Exception as e:
             if debug_mode:
